e80fa7fd7109e6b86cc4a28c6359a3cd58eba135
Myeonghun Jeong <myeonghunjeong@Myeonghuns-MacBook-Pro.local>
2023-03-06 19:01:57 -0600

    added footer-page

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer(){
+return (
+ <div className="portfolio-footer" id="footer">
+    <a href="https://github.com/mjtic/"><img src="https://img.icons8.com/ios/80/000000/github.png" alt="github-logo" class="icon"/></a>
+    <a href="https://www.linkedin.com/in/myeonghun-jeong-7b9193b4/"><img src="https://img.icons8.com/fluent/80/000000/linkedin-circled.png" alt="linkedin-logo" class="icon"/></a>
+    <a href="https://stackoverflow.com/users/20760901/myeonghun-jeong"><img src="https://img.icons8.com/ios/80/000000/stackoverflow.png" alt="stack-overflow" class="icon"/></a>
+ </div>
+);
+
+}
